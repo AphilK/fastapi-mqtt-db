686ac98bedcf3060d18e74241360bf1c3868aa1b
AphilK <joaosilvanascimento.jops@gmail.com>
2026-09-18 12:11:40 -0300

    Minor changes (random client_id)

--- mqtt.py
+++ mqtt.py
@@ -1,15 +1,13 @@
 import paho.mqtt.client as mqtt
-
-TOPIC = "#"
+import random
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe(TOPIC)
 
 def on_message(client, userdata, msg):
     print(f"{msg.topic}: {msg.payload.decode()}")
 
-client = mqtt.Client()
+client = mqtt.Client(client_id=f"subscriber-{random.randint(0, 1000)}")
 # client.username_pw_set("username", "password")  # If using auth
 client.on_connect = on_connect
 client.on_message = on_message
